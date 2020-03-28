1839c906692c01b7551b4f787e3ff23fdc2703e0
Francis Badasu <nytro04@gmail.com>
2020-03-28 01:41:12 +0000

    try catch async refactor

--- controllers/guardController.js
+++ controllers/guardController.js
@@ -1,5 +1,7 @@
 const Guard = require("../models/guardModel");
 const APIFeatures = require("./../utils/apiFeatures");
+const catchAsync = require("./../utils/catchAsync");
+
 /**
  * Guards Routes Handler Functions or Controllers
  * handles routes to the various guards endpoints
@@ -20,227 +22,205 @@ exports.aliasTop = (req, res, next) => {
   next();
 };
 
-exports.getAllGuards = async (req, res) => {
-  try {
-    // another way to filter, you can use lt,gt,lte,gte
-    // const guards = await Guard.find()
-    //   .where("name")
-    //   .equals("koo")
-    //   .where("title")
-    //   .equals("Mr");
+// Get all Guards //todo: implement filtering
+exports.getAllGuards = catchAsync(async (req, res, next) => {
+  // another way to filter, is use lt,gt,lte,gte
+  // const guards = await Guard.find()
+  //   .where("name")
+  //   .equals("koo")
+  //   .where("title")
+  //   .equals("Mr");
 
-    //req.query is used to access queries(optional params) for filtering
+  //req.query is used to access queries(optional params) for filtering
 
-    // execute query
-    // const features = new APIFeatures(Guard.find(), req.query)
-    //   .filter()
-    //   .sort()
-    //   .limitFields()
-    //   .paginate();
+  // execute query
+  // const features = new APIFeatures(Guard.find(), req.query)
+  //   .filter()
+  //   .sort()
+  //   .limitFields()
+  //   .paginate();
 
-    // const guards = await features.query;
+  // const guards = await features.query;
 
-    const guards = await Guard.find();
+  const guards = await Guard.find();
 
-    res.status(200).json({
-      status: "success",
-      length: guards.length,
-      data: {
-        guards
-      }
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "failed",
-      message: err
-    });
-  }
-};
+  res.status(200).json({
+    status: "success",
+    length: guards.length,
+    data: {
+      guards
+    }
+  });
+});
 
-exports.getGuard = async (req, res) => {
-  try {
-    // Guard.findOne({ _id: req.params.id}) same as findById
+// Get Single Guard
+exports.getGuard = catchAsync(async (req, res, next) => {
+  // Guard.findOne({ _id: req.params.id}) same as findById
 
-    const guard = await Guard.findById(req.params.id);
+  const guard = await Guard.findById(req.params.id);
 
-    res.status(200).json({
-      status: "success",
-      data: {
-        guard
-      }
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "failed",
-      message: err
-    });
-  }
-};
+  res.status(200).json({
+    status: "success",
+    data: {
+      guard
+    }
+  });
+});
 
-exports.createGuard = async (req, res) => {
-  try {
-    // we can create a new guard document like this
-    // const newGuard = new Guard({})
-    // newGuard.save()
+// Create new Guard
+exports.createGuard = catchAsync(async (req, res, next) => {
+  /**
+   * newGuard creates a new guard document
+   * with data from the request body.
+   * any field passed to the request body
+   * but is not in the DB schema is ignored by the DB.
+   * @param {object} req.body
+   *
+   * save or create returns a promise, this is handled
+   * by adding .then or async to the function eg. async (req, res)
+   * and awaiting the response
+   */
+  const newGuard = await Guard.create(req.body);
 
-    /**
-     * newGuard creates a new guard document
-     * with data from the request body.
-     * any field passed to the request body
-     * but is not in the DB schema is ignored by the DB.
-     * @param {object} req.body
-     *
-     * save or create returns a promise, this is handled
-     * by adding .then or async to the function eg. async (req, res)
-     * and awaiting the response
-     */
-    const newGuard = await Guard.create(req.body);
+  res.status(201).json({
+    status: "success",
+    data: {
+      guard: newGuard
+    }
+  });
+});
 
-    res.status(201).json({
-      status: "success",
-      data: {
-        guard: newGuard
-      }
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: "fail",
-      message: err
-    });
-  }
-};
+// Update a Guard
+exports.updateGuard = catchAsync(async (req, res, next) => {
+  const guard = await Guard.findByIdAndUpdate(req.params.id, req.body, {
+    new: true, // returns the new document instead of the old one
+    runValidators: true // runs validators against the model's schema
+  });
 
-exports.updateGuard = async (req, res) => {
-  try {
-    const guard = await Guard.findByIdAndUpdate(req.params.id, req.body, {
-      new: true, // returns the new document instead of the old one
-      runValidators: true // runs validators against the model's schema
-    });
+  res.status(200).json({
+    status: "success",
+    data: {
+      guard
+    }
+  });
+});
 
-    res.status(200).json({
-      status: "success",
-      data: {
-        guard
-      }
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "failed",
-      message: err
-    });
-  }
-};
-
-exports.deleteGuard = async (req, res) => {
+exports.deleteGuard = catchAsync(async (req, res, next) => {
   await Guard.findByIdAndDelete(req.params.id);
 
-  try {
-    res.status(204).json({
-      status: "success",
-      data: null
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "failed",
-      message: err
-    });
-  }
-};
+  res.status(204).json({
+    status: "success",
+    data: null
+  });
+});
 
 //Mongo pipeline aggregation = some computation, allows for the manipulation of data is some ways
-exports.getGuardStats = async (req, res) => {
-  try {
-    const stats = await Guard.aggregate([
-      {
-        $match: { salary: { gte: 500 } } // match is used to select documents
-      },
-      {
-        $group: {
-          // _id: "$salary", // get you all the stats list below about salary
-          _id: null,
-          numGuards: { $sum: 1 }, //total number of guards
-          totalSalary: { $sum: "$salary" }, // sum of guards salary
-          avgAge: { $avg: "$age" }, // average age
-          avgSalary: { $avg: "$salary" }, // average salary
-          minSalary: { $min: "$salary" }, // minimum salary
-          maxSalary: { $max: "$salary" } // maximum salary
-        }
-      },
-      {
-        $sort: { minSalary: 1 } // sort minimum salary by ascending(lowest first)
-      },
-      // just to show we can repeat stages in the aggregation pipeline
-      {
-        $match: { _id: { $ne: "male" } } // match all document that's not equal to male
+exports.getGuardStats = catchAsync(async (req, res, next) => {
+  const stats = await Guard.aggregate([
+    {
+      $match: { salary: { gte: 500 } } // match is used to select documents
+    },
+    {
+      $group: {
+        // _id: "$salary", // get you all the stats list below about salary
+        _id: null,
+        numGuards: { $sum: 1 }, //total number of guards
+        totalSalary: { $sum: "$salary" }, // sum of guards salary
+        avgAge: { $avg: "$age" }, // average age
+        avgSalary: { $avg: "$salary" }, // average salary
+        minSalary: { $min: "$salary" }, // minimum salary
+        maxSalary: { $max: "$salary" } // maximum salary
       }
-    ]);
+    },
+    {
+      $sort: { minSalary: 1 } // sort minimum salary by ascending(lowest first)
+    },
+    // just to show we can repeat stages in the aggregation pipeline
+    {
+      $match: { _id: { $ne: "male" } } // match all document that's not equal to male
+    }
+  ]);
 
-    res.status(200).json({
-      status: "success",
-      data: {
-        stats
-      }
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "failed",
-      message: err
-    });
-  }
-};
+  res.status(200).json({
+    status: "success",
+    data: {
+      stats
+    }
+  });
+});
 
-exports.getMonthlyPlan = async (req, res) => {
-  try {
-    const year = req.params.year * 1;
+exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
+  const year = req.params.year * 1;
 
-    const plan = await Guard.aggregate([
-      {
-        $unwind: "$age" // unwind spreads an array
-      },
-      {
-        $match: {
-          age: {
-            $gte: new Date(`${year}-01-01`),
-            $lte: new Date(`${year}-12-01`)
-          }
+  const plan = await Guard.aggregate([
+    {
+      $unwind: "$age" // unwind spreads an array
+    },
+    {
+      $match: {
+        age: {
+          $gte: new Date(`${year}-01-01`),
+          $lte: new Date(`${year}-12-01`)
         }
-      },
-      {
-        $group: {
-          _id: { $month: "$age" },
-          numOfAge: { $sum: 1 },
-          guards: { $push: "$name" }
-        }
-      },
-      {
-        // adds a new field to the result
-        $addFields: { month: "$_id" }
-      },
-      {
-        // adds or remove a field, 0 or 1
-        $project: {
-          _id: 0
-        }
-      },
-      {
-        // used for sorting, 1 ascending, -1 descending
-        $sort: { someName: -1 }
-      },
-      {
-        $limit: 6 // limits the output to 6
       }
-    ]);
+    },
+    {
+      $group: {
+        _id: { $month: "$age" },
+        numOfAge: { $sum: 1 },
+        guards: { $push: "$name" }
+      }
+    },
+    {
+      // adds a new field to the result
+      $addFields: { month: "$_id" }
+    },
+    {
+      // adds or remove a field, 0 or 1
+      $project: {
+        _id: 0
+      }
+    },
+    {
+      // used for sorting, 1 ascending, -1 descending
+      $sort: { someName: -1 }
+    },
+    {
+      $limit: 6 // limits the output to 6
+    }
+  ]);
 
-    res.status(200).json({
-      status: "success",
-      data: {
-        stats
-      }
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "failed",
-      message: err
-    });
-  }
-};
+  res.status(200).json({
+    status: "success",
+    data: {
+      stats
+    }
+  });
+});
+
+/**
+ * This was my try catch block of a controller before
+ * refactoring to make the code DRY
+ *  all the code is wrapped in a try catch block
+ *
+ */
+
+// exports.getGuard = async (req, res, next) => {
+//   try {
+//     // Guard.findOne({ _id: req.params.id}) same as findById
+
+//     const guard = await Guard.findById(req.params.id);
+
+//     res.status(200).json({
+//       status: "success",
+//       data: {
+//         guard
+//       }
+//     });
+//   } catch (err) {
+//     res.status(404).json({
+//       status: "failed",
+//       message: err
+//     });
+//   }
+// };
