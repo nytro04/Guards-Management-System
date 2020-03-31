fb5061a8f1bf3c5f4a6d0095449fc01e46276aa3
Francis Badasu <nytro04@gmail.com>
2020-03-31 02:16:29 +0000

    added uncaught and unhandled exceptions

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+
+// Handle Uncaught Exception
+process.on("uncaughtException", err => {
+  console.log("UNCAUGHT EXCEPTION...");
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 // gives you access to all environment variables
 dotenv.config({ path: "./config.env" });
 
@@ -11,10 +19,21 @@ mongoose
     useCreateIndex: true,
     useFindAndModify: false
   })
-  .then(( ) => console.log("DB Connection successful... 🔥🔥🔥"));
+  .then(() => console.log("DB Connection successful... 🔥🔥🔥"));
 
 // start serve and listen on port
 const port = process.env.PORT || 8000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}... 🚀🚀🚀`);
+});
+
+/** Unhandled Rejections
+ * Async Errors that were not dealt with
+ */
+process.on("unhandledRejection", err => {
+  console.log(err.name, err.message);
+  console.log("UNHANDLED REJECTION ERROR");
+  server.close(() => {
+    process.exit(1);
+  });
 });
