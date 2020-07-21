1a8084970627b8d90e2435d5d782a741dd963253
Francis Badasu <nytro04@gmail.com>
2020-07-21 03:08:29 +0000

    refactor: sort, limit fields and pagination

--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -15,12 +15,13 @@ class APIFeatures {
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
-    this.query = this.query.find(JSON.parse(queryStr)); 
+    this.query = this.query.find(JSON.parse(queryStr));
 
     return this;
   }
 
   sort() {
+    // adding - to the select query sorts results in ascending order
     if (this.queryString.sort) {
       const sortBy = this.queryString.sort.split(",").join(" ");
       this.query = this.query.sort(sortBy);
@@ -33,7 +34,7 @@ class APIFeatures {
 
   limitFields() {
     // 3. Field limiting. send back only fields requested by clients aka projecting
-    // adding - to the select query removes or hides the field it from the data sent back
+    // adding - to the select query removes or hides the field from the data sent back
     if (this.queryString.fields) {
       const fields = this.queryString.fields.split(",").join(" ");
       this.query = this.query.select(fields);
@@ -47,11 +48,13 @@ class APIFeatures {
   paginate() {
     // 4. Pagination
     // if page=3,&limit=10, page 1, 1-10, page 2, 11 - 20, page 3, 21 - 30
-    const page = this.queryString.page * 1 || 1; // returns the requested page or defaults to 1
+    const page = this.queryString.page * 1 || 1; // returns the requested page or defaults to 1. multiplying a string by 1 converts it into a number
     const limit = this.queryString.limit * 1 || 20;
     const skip = (page - 1) * limit;
 
     this.query = this.query.skip(skip).limit(limit);
+
+  
 
     return this;
   }
