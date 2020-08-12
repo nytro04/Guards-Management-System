7c22ebf45dca3e703db36b8403a4967a43053ef8
Francis Badasu <nytro04@gmail.com>
2020-08-12 21:20:05 +0000

    feat: routes for clients get,update and delete client

--- routes/clientRoutes.js
+++ routes/clientRoutes.js
@@ -2,12 +2,22 @@ const express = require("express");
 const {
   createClient,
   getAllClients,
+  getClient,
+  updateClient,
+  deleteClient,
 } = require("../controllers/clientController");
-const { protect } = require("../controllers/authController");
+const { protect, restrictTo } = require("../controllers/authController");
 
 const router = express.Router();
 
 //Routes for get all clients and create client
 router.route("/").get(protect, getAllClients).post(createClient);
+
+//Routes for getSingle client, update and delete client
+router
+  .route("/:id")
+  .get(getClient)
+  .patch(updateClient)
+  .delete(protect, restrictTo("admin-bd"), deleteClient);
 
 module.exports = router;
