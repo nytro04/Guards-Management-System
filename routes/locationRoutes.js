24352734d4299cf743d90bda938f5298dc92b882
Francis Badasu <nytro04@gmail.com>
2020-09-08 20:41:48 +0000

    feat: add get,update and delete location routes

--- routes/locationRoutes.js
+++ routes/locationRoutes.js
@@ -1,15 +1,24 @@
-const express = require("express");
+const express = require("express")
 const {
   createLocation,
   getAllLocations,
-} = require("../controllers/locationController");
-const { protect, restrictTo } = require("../controllers/authController");
+  getLocation,
+  updateLocation,
+  deleteLocation,
+} = require("../controllers/locationController")
+const { protect, restrictTo } = require("../controllers/authController")
 
-const router = express.Router();
+const router = express.Router()
 
 router
   .route("/")
   .get(getAllLocations)
-  .post(protect, restrictTo("admin"), createLocation);
+  .post(protect, restrictTo("admin"), createLocation)
 
-module.exports = router;
+router
+  .route("/:id")
+  .get(getLocation)
+  .patch(protect, restrictTo("admin"), updateLocation)
+  .delete(protect, restrictTo("admin"), deleteLocation)
+
+module.exports = router
